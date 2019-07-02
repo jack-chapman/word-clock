ead807499476e10d9971c751da28d48c6f4a94f2
jack-chapman <jackchapman093@gmail.com>
2019-07-02 20:21:30 +0100

    Added working clock

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,122 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="container">
+      <letter
+        v-for="(l, index) in list"
+        :key="index"
+        :character="l"
+        :isOn="indexes.includes(index)"
+      />
+    </div>
+    <div class="time">
+      <p>{{now}}</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Letter from './components/Letter.vue';
+import LetterList from './components/letterList';
 
 export default Vue.extend({
   name: 'app',
   components: {
-    HelloWorld
+    Letter
+  },
+  data() {
+    return {
+      now: '',
+      hours: 0,
+      minutes: 0,
+      intervalId: 0
+    };
+  },
+  computed: {
+    list(): string[] {
+      return LetterList;
+    },
+    roundedMinute(): number {
+      return Math.floor(this.minutes / 5) * 5;
+    },
+    minuteIndexes(): number[] {
+      switch (this.roundedMinute) {
+        default:
+        case 0:
+          return [105, 106, 107, 108, 109, 110];
+        case 5:
+          return [28, 29, 30, 31, 44, 45, 46, 47];
+        case 10:
+          return [38, 39, 40, 44, 45, 46, 47];
+        case 15:
+          return [13, 14, 15, 16, 17, 18, 19, 44, 45, 46, 47];
+        case 20:
+          return [22, 23, 24, 25, 26, 27, 44, 45, 46, 47];
+        case 25:
+          return [22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 44, 45, 46, 47];
+        case 30:
+          return [33, 34, 35, 36, 44, 45, 46, 47];
+        case 35:
+          return [22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 42, 43];
+        case 40:
+          return [22, 23, 24, 25, 26, 27, 42, 43];
+        case 45:
+          return [13, 14, 15, 16, 17, 18, 19, 42, 43];
+        case 50:
+          return [38, 39, 40, 42, 43];
+        case 55:
+          return [28, 29, 30, 31, 42, 43];
+      }
+    },
+    hourIndexes(): number[] {
+      const hour =
+        this.hours > 12 ? this.hours - 12 : this.hours === 0 ? 12 : this.hours;
+      switch (hour) {
+        default:
+        case 1:
+          return [55, 56, 57];
+        case 2:
+          return [74, 75, 76];
+        case 3:
+          return [61, 62, 63, 64, 65];
+        case 4:
+          return [66, 67, 68, 69];
+        case 5:
+          return [70, 71, 72, 73];
+        case 6:
+          return [58, 59, 60];
+        case 7:
+          return [88,89, 90, 91, 92];
+        case 8:
+          return [77, 78, 79, 80];
+        case 9:
+          return [51, 52, 53, 54];
+        case 10:
+          return [99, 100, 101];
+        case 11:
+          return [82, 83, 84, 85, 86, 87];
+        case 12:
+          return [93, 94, 95, 96, 97, 98];
+      }
+    },
+    indexes(): number[] {
+      return [0, 1, 3, 4, ...this.minuteIndexes, ...this.hourIndexes];
+    }
+  },
+  mounted() {
+    this.getCurrentTime();
+    this.intervalId = setInterval(this.getCurrentTime, 1000);
+  },
+  destroyed() {
+    clearInterval(this.intervalId);
+  },
+  methods: {
+    getCurrentTime(): void {
+      const d = new Date();
+      this.now = d.toLocaleTimeString('en-GB');
+      this.hours = d.getHours();
+      this.minutes = d.getMinutes();
+    }
   }
 });
 </script>
@@ -24,6 +128,19 @@ export default Vue.extend({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.container {
+  display: grid;
+  grid-template: repeat(11, 20px) / repeat(10, 20px);
+}
+
+.time {
+  margin-top: 20px;
 }
 </style>
---
+++ src/components/Letter.vue
@@ -0,0 +1,33 @@
+<template>
+  <p :class="classes">{{character}}</p>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'letter',
+  props: {
+    character: {
+      type: String,
+      required: true
+    },
+    isOn: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    classes(): object {
+      return {
+        'on': this.isOn
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.on {
+  color: coral;
+}
+</style>
